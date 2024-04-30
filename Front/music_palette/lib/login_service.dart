6c7d797836413452a0a183d5980328a7b157a035
UmbaDumba <dkdi3104@gmail.com>
2024-04-30 14:46:56 +0900

    front commit

---
+++ Front/music_palette/lib/login_service.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:music_palette/api_sevice.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MusicUser {
+  late GoogleSignInAccount googleUser;
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  late String userImage;
+  late String userName;
+  late String userEmail;
+  late String userId;
+  bool isLogin = false;
+
+  late List<bool> likeList;
+
+  void updateuser(
+      {required GoogleSignInAccount googleaccount, required String uID}) {
+    userImage = googleaccount.photoUrl!;
+    userName = googleaccount.displayName!;
+    userEmail = googleaccount.email;
+    userId = uID;
+    isLogin = true;
+  }
+
+  Future<void> setLike(Future<List<bool>> futureLikeList) async {
+    List<bool> returnList = [];
+    returnList = await futureLikeList;
+
+    likeList = returnList;
+    //print(likeList);
+  }
+
+  Future<void> autoLogin(User user) async {
+    Future<List<bool>> futurelikeList = ApiService.getLikeMusics(user.uid);
+
+    await setLike(futurelikeList);
+
+    userImage = user.photoURL!;
+    userName = user.displayName!;
+    userEmail = user.email!;
+    userId = user.uid;
+    isLogin = true;
+  }
+
+  Future<void> logout() async {
+    await auth.signOut();
+    await GoogleSignIn().signOut();
+    isLogin = false;
+  }
+
+  Future<void> signInWithGoogle() async {
+    //googleSignIn googleSignIn = GoogleSignIn();
+    //print("test");
+    final GoogleSignInAccount? googleuser = await GoogleSignIn().signIn();
+
+    if (googleuser == null) {
+      //print("error");
+      return;
+    }
+
+    GoogleSignInAuthentication authentication = await googleuser.authentication;
+    AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: authentication.accessToken,
+      idToken: authentication.idToken,
+    );
+
+    UserCredential authresult =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+    //User authUser = authresult.user!;
+
+    if (authresult.user != null) {
+      //print(authresult.user!.uid);
+      Future<List<bool>> futurelikeList =
+          ApiService.getLikeMusics(authresult.user!.uid);
+      //print("test");
+
+      await setLike(futurelikeList);
+    }
+
+    updateuser(googleaccount: googleuser, uID: authresult.user!.uid);
+  }
+}
